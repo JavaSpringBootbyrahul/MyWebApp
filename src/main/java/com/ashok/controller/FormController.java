d14537653ce4ef6cc6771e22173a7ead0af8882f
RAHUL-PC\Rzope <rzope5923@gmail.com>
2023-02-18 18:16:54 +0530

    commit to local

---
+++ src/main/java/com/ashok/controller/FormController.java
@@ -0,0 +1,26 @@
+package com.ashok.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class FormController {
+	
+	@GetMapping("/")
+	public String registerForm()
+	{
+		return "register";
+	}
+	
+	@PostMapping("/save")
+	public String submitForm( @RequestParam("name")String name, @RequestParam("age") int age,Model model)
+	{
+		model.addAttribute("name",name);
+		model.addAttribute("age",age);
+		return "result";
+	}
+}
